d14cff4826f5a1a7e340da74164029d90cbd7c12
Hardiansyah <hardiansyahmail@gmail.com>
2026-01-05 16:11:12 +0700

    update : generate ul master ppt template

--- main2.py
+++ main2.py
@@ -43,19 +43,57 @@ def read_and_write_ppt():
         for p in slide.placeholders:
             print(p.placeholder_format.idx, p.name)
 
-        text_top_shape = slide.placeholders[0]
-        text_bottom_shape = slide.placeholders[1]
+        title = None
+        title_description = None
+        picture = None
+        container1 = None
+        container2 = None
 
-        set_bullet_list(text_top_shape, text_top)
-        set_bullet_list(text_bottom_shape, text_bottom)
-
-        # replace image
         for shape in slide.shapes:
-            if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
-                left, top, width, height = shape.left, shape.top, shape.width, shape.height
-                slide.shapes._spTree.remove(shape._element)
-                slide.shapes.add_picture("new_image.png", left, top, width=width, height=height)
-                break
+
+            if shape.has_text_frame and shape.text == 'container1':
+                container1 = shape
+
+            if shape.has_text_frame and shape.text == 'container2':
+                container2 = shape
+
+            if shape.has_text_frame and shape.text == 'judul':
+                shape.text = 'Update Kondisi Siklon Tropis dan Bibit Siklon Tropis'
+                title = shape
+
+            if shape.has_text_frame and shape.text == 'tanggal':
+                shape.text = f"{today}"
+                title_description = shape
+
+            if not shape.has_text_frame and shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
+                picture = shape
+
+            if shape.has_text_frame:
+                print(
+                    shape.name,
+                    "| placeholder:",
+                    shape.is_placeholder,
+                    "| has_text:",
+                    shape.has_text_frame,
+                    "| text:",
+                    shape.text
+                )
+            else:
+                print(
+                    shape.name,
+                    "| placeholder:",
+                    shape.is_placeholder,
+                    "| has_text:",
+                    shape.has_text_frame
+                )
+
+        set_bullet_list(container1, text_top)
+        set_bullet_list(container2, text_bottom)
+
+        if picture:
+            left, top, width, height = picture.left, picture.top, picture.width, picture.height
+            slide.shapes._spTree.remove(shape._element)
+            slide.shapes.add_picture("new_image.png", left, top, width=width, height=height)
 
         prs.save("output.pptx")
     except Exception as e:
